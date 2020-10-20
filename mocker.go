fa12f90e4c1c9dd1ef6686f5bb44380ad2963151
Charles Read <charles@charlesread.io>
2020-10-20 11:39:28 -0400

    added dockerfile and more clone flags

--- mocker.go
+++ mocker.go
@@ -1,23 +1,45 @@
+// +build linux
+
 package main
 
 import (
 	"fmt"
 	"os"
 	"os/exec"
+	"syscall"
 )
 
 func main() {
 
-	cmd := exec.Command("/bin/sh")
+	command := "/bin/sh"
+
+	env := []string{
+		"PS1=shell > ",
+	}
+
+	cloneFlags := syscall.CLONE_NEWNS |
+		syscall.CLONE_NEWUTS |
+		syscall.CLONE_NEWIPC |
+		syscall.CLONE_NEWPID |
+		syscall.CLONE_NEWNET |
+		syscall.CLONE_NEWUSER
+
+	sysProcAttr := &syscall.SysProcAttr{
+		Cloneflags: uintptr(cloneFlags),
+	}
+
+	cmd := exec.Command(command)
 
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+	cmd.Env = env
+	cmd.SysProcAttr = sysProcAttr
 
 	err := cmd.Run()
 
 	if err != nil {
-		fmt.Printf("Error encountered running command: %s\n", err.Error())
+		fmt.Printf("Error encountered running command %q: %s\n", command, err.Error())
 		os.Exit(1)
 	}
 
